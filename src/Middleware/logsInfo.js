5b7c5c6ab54e025dc76cb9898a9bf465348739a2
rezaadit12 <rezaadit410@gmail.com>
2025-02-12 23:39:42 +0700

    (feat) add integration test for book

--- src/Middleware/logsInfo.js
+++ src/Middleware/logsInfo.js
@@ -1,6 +1,11 @@
 import chalk from 'chalk';
 
 const logRequest = (req, res, next) => {
+
+    if (process.env.NODE_ENV === "test") {
+        return next(); // Skip logging saat testing
+    }
+
     const start = Date.now();
 
     res.on('finish', () => {
